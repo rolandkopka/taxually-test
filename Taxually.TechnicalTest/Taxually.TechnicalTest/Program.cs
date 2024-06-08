4bc4c3b4164d096576198e0b4f485087f80134c6
Roland Kopka <kopkaroland@gmail.com>
2024-06-08 12:01:35 +0200

    Add better enum handling settings

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
@@ -4,10 +4,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.AllowInputFormatterExceptionMessages = false;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.UseInlineDefinitionsForEnums();
+});
 
 builder.Services.AddScoped<VatRegistrationServiceGb>();
 builder.Services.AddScoped<VatRegistrationServiceFr>();
